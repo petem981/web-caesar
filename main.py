a4b139c0844714fe38fd6867c2c79fed612e8456
Mary Peterson <the.airforce.captain@gmail.com>
2018-03-26 17:43:42 -0500

    added text input boxes

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- create your form here -->
+def form(): 
+    content = """
+        <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0">
+                <p class="error"></p>
+            </div>
+            <textarea type="text" name="text"></textarea>
+            <br>
+            <input type="submit">
+        </form> """
+    return content
+    </body>
+</html>
+
+@app.route("/")
+app.run()
